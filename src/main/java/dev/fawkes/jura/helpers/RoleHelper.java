39cb49a9dbb3de56ccb6d6ce3cfdf1e10491dc6c
BlueDragon709 <brian40@live.nl>
2020-04-06 19:00:52 +0200

    ✨ Add remove streaming role from all users

--- src/main/java/dev/fawkes/jura/helpers/RoleHelper.java
+++ src/main/java/dev/fawkes/jura/helpers/RoleHelper.java
@@ -1,10 +1,13 @@
 package dev.fawkes.jura.helpers;
 
+import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.entities.Guild;
 import net.dv8tion.jda.api.entities.Member;
 import net.dv8tion.jda.api.entities.Role;
 
 import java.util.List;
+
+import static dev.fawkes.jura.FawkesApplicationRunner.CURRENT_STREAMING_ROLE_ID_PROPERTY_NAME;
 
 public class RoleHelper {
     /**
@@ -42,6 +45,25 @@ public class RoleHelper {
             if (memberRole == roleToRemove) {
                 guild.removeRoleFromMember(member, roleToRemove).complete();
                 break;
+            }
+        }
+    }
+
+    /**
+     * Remove the streaming role from all users that have the role in the guild.
+     * @param jda The bot.
+     * @param channelId A channel of the current guild.
+     */
+    public static void removeStreamingRoleFromAllMembers(JDA jda, String channelId) {
+        String streamingRoleId = System.getenv().get(CURRENT_STREAMING_ROLE_ID_PROPERTY_NAME);
+
+        if (streamingRoleId != null && !streamingRoleId.isEmpty()) {
+            Guild guild = jda.getTextChannelById(channelId).getGuild();
+            List<Member> members = guild.getMembers();
+            Role streamingRole = guild.getRoleById(streamingRoleId);
+
+            for (Member member : members) {
+                RoleHelper.removeRole(streamingRole, member, guild);
             }
         }
     }
